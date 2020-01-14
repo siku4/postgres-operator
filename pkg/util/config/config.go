b381de1e1be28445e6c1022053b16a4f8994d059
Dmitrii Dolgov <9erthalion6@gmail.com>
2020-01-14 15:59:16 +0100

    And example of configuration

--- pkg/util/config/config.go
+++ pkg/util/config/config.go
@@ -86,7 +86,7 @@ type ConnectionPool struct {
 	Schema            string `name:"connection_pool_schema" default:"pooler"`
 	User              string `name:"connection_pool_user" default:"pooler"`
 	Type              string `name:"connection_pool_type" default:"pgbouncer"`
-	Image             string `name:"connection_pool_image"  default:"pierone.stups.zalan.do/acid/pgbouncer:0.0.1"`
+	Image             string `name:"connection_pool_image"  default:"pgbouncer:1.0"`
 	Mode              string `name:"connection_pool_mode"  default:"session"`
 }
 
